e3942273a8fb6244760d4d1972e0d8078a48cd2b
Martin Stievenart <stievenart.m@gmail.com>
2022-11-29 10:09:50 +0100

    feat: part two of day one

--- 2021/1.js
+++ 2021/1.js
@@ -1,4 +1,4 @@
-const main = (measurements) => {
+const partOne = (measurements) => {
     let result = 0;
     measurements.reduce((prev, current) => {
         if (current > prev) {
@@ -7,5 +7,31 @@ const main = (measurements) => {
 
         return current;
     });
+    return result;
+}
+
+const partTwo = (measurements) => {
+    let result = 0;
+    let input = [];
+    let sums = [];
+
+    measurements.reduce((prev, current, index) => {
+        input.push(current);
+
+        if (index >= 2) {
+            sums.push(input.reduce((a, b) => a + b, 0));
+        
+            input.shift();
+
+            if (sums.length >= 2) {
+                if (sums[1] > sums[0]) {
+                    result++;
+                }
+
+                sums.shift();
+            }
+        }
+    }, 0);
+
     return result;
 }
